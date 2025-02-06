ee6eb4b22f5a49a7b1163ea7dead888116a9b59e
Maria Kolesnikova <maria.wheels28@gmail.com>
2025-02-05 18:28:19 -0800

    Replace constants, change counts comments functional

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.conf import settings
 from django.urls import reverse
 
 from blog.constants import MAX_LENGTH, MAX_LENGTH_SLUG
